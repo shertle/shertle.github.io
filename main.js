2159cb9dd4ef8941eac356bdbeffd6866947b261
Sherman Cheung <shermancheung314@gmail.com>
2015-08-06 21:19:37 -0700

    Add jquery link and start main.js

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-// Controls Fading of Menu Bar
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+// Check that jQuery is working properly
+/*window.onload = function() {
+    alert( "welcome" ); 
+};
+*/
 
-    if (scroll >= 50) {
-        $("nav").addClass("navOpac");
-    } else {
-        $("nav").removeClass("navOpac");
-    }
+    $("body").click(function() {
+        alert( "Handler for .click() called." );
+    });
+
+$(document).ready(function() {
+
+    // Controls Fading of Menu Bar
+    $("section").scroll(function() {    
+        var scroll = $(window).scrollTop();
+        alert("scrolled");
+        console.log('scrolled');
+
+        if (scroll >= 5) {
+            $("nav").addClass("navOpac");
+        } 
+        else {
+            $("nav").removeClass("navOpac");
+        }
+
+    });
+
+    $("body").click(function() {
+        alert( "Handler for .click() called." );
+    });
 });
