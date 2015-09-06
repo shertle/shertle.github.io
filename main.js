1930cd525f2e5ca44c2711b51cf7ea5c2c6a339f
Sherman Cheung <shermancheung314@gmail.com>
2015-09-06 13:14:34 -0700

    Fix nav-bar opacity on scroll for different screen sizes

--- main.js
+++ main.js
@@ -6,27 +6,42 @@
   Your welcome.
 */
 
-// Check that jQuery is working properly
-/*
-window.onload = function() {
-    alert( "welcome" ); 
-};
-*/
-
 // Apply JS only after HTML finishes loading
 $(document).ready(function() {
     //console.log("works woohoo!");
 
     // Darken Nav Bar after scrolling down
     $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
+      var scroll = $(window).scrollTop();
 
-        if (scroll >= 50 && scroll <= 1200) {
-            $("nav").addClass("nav-opac");
+      // Mobile with small screen height
+      if ($(window).height() < 490) {
+        if (scroll >= 50 && scroll <= 1100) {
+          $("nav").addClass("nav-opac");
         } 
         else {
-            $("nav").removeClass("nav-opac");
+          $("nav").removeClass("nav-opac");
         }
+      }
+      // Shrunken desktop and mobile layout
+      else if ($(window).width() < 800) {
+        if (scroll >= 50 && scroll <= 1350) {
+          $("nav").addClass("nav-opac");
+        } 
+        else {
+          $("nav").removeClass("nav-opac");
+        }
+      }
+      // Regular desktop
+      else {
+        if (scroll >= 50 && scroll <= 1200) {
+          $("nav").addClass("nav-opac");
+        } 
+        else {
+          $("nav").removeClass("nav-opac");
+        }
+      }
+
     });
 
     // Enable smooth scroll from nav-bar links
